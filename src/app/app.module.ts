02dba4064a37d49f9e7f0ea7d0c81d1bb7fffeba
Pablo Velez <pablovv2012@gmail.com>
2018-05-20 15:58:02 +0300

    Add the flexlayout new angular library for create flexible and responsive layout with almost no css implied

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { MyOwnCustomMaterialModule } from './modules/material.module';
+import { FlexLayoutModule } from "@angular/flex-layout";
 
 import { AppComponent } from './app.component';
 
@@ -13,6 +14,7 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    FlexLayoutModule,
     BrowserAnimationsModule,
     MyOwnCustomMaterialModule
   ],
