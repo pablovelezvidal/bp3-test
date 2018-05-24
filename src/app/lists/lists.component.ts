8da627d5183526d11a809f7b333635e2c09cc769
Pablo Velez <pablovv2012@gmail.com>
2018-05-25 00:38:03 +0300

    Complete the filterin functionality for the nodes, in both, cards and json

--- src/app/lists/lists.component.ts
+++ src/app/lists/lists.component.ts
@@ -9,18 +9,22 @@ import { Edge } from '../models/Edge';
   templateUrl: './lists.component.html',
   styleUrls: ['./lists.component.scss']
 })
+
 export class ListsComponent implements OnInit {
 
-  nodes : Array<Node>;
-  edges : Array<Edge>;
-  entireResponse: Array<any>;
+  completeJson;
+
   endpoints : Array<any> = [
     {name: "1 - Simple process", path: "data/1-simple-process"},
     {name: "2 - Multiple human services", path: "data/2-multiple-human-services"},
     {name: "3 - Branching process", path: "data/3-branching-process"},
     {name: "4 - Recursive branching process", path: "data/4-recursive-branching-process"}
   ];
+
   selectedEndpoint: string = "data/3-branching-process";
+
+  //filter to human task boolean
+  filterTHT: boolean = false;
 
   constructor(private _dataService: DataService) { }
 
@@ -36,10 +40,32 @@ export class ListsComponent implements OnInit {
   fetchJsonData() {
     this._dataService.getList(this.selectedEndpoint)
       .subscribe(res => {
-        this.entireResponse = res;
-        this.nodes = res.nodes; 
-        this.edges = res.edges
+        this.completeJson = res;
+        if (this.filterTHT) {
+          this.filterNodesTHT();
+        }
       });
   }
 
+  filterToHumanTask(event) {
+    this.filterTHT = event.checked;
+    this.fetchJsonData();
+  }
+
+  filterNodesTHT() {
+    this.completeJson.nodes = this.completeJson.nodes.filter( node => {
+      if (node.type == "ServiceTask") {
+        this.filterEdgeTHT(node);
+      } else {
+        return node;
+      }
+    });
+  }
+
+  filterEdgeTHT(node) {
+    // let index = this.completeJson.indexOf(node.)
+    // this.completeJson.edges.splice();
+  }
+
 }
+
