4dd4d6fa5d2c8a8f8ff29362dcb546b81a767066
Pablo Velez <pablovv2012@gmail.com>
2018-05-26 13:57:42 +0300

    make the app more beauty

--- src/app/modules/material.module.ts
+++ src/app/modules/material.module.ts
@@ -1,5 +1,5 @@
 import { 
-  MatButtonModule, MatIconModule, MatInputModule, 
+  MatButtonModule, MatIconModule, MatInputModule, MatChipsModule,
   MatSelectModule, MatSliderModule, MatToolbarModule, MatTabsModule, MatRadioModule,
   MatCardModule, MatSlideToggleModule, MatSnackBarModule, MatCheckboxModule, MatSidenavModule } from '@angular/material';
 
@@ -8,10 +8,10 @@ import { NgModule } from '@angular/core';
 @NgModule({
   imports: [MatButtonModule, MatCheckboxModule, MatButtonModule, MatIconModule, MatInputModule, 
     MatSelectModule, MatSliderModule, MatToolbarModule, MatTabsModule, MatRadioModule,
-    MatCardModule, MatSlideToggleModule,
+    MatCardModule, MatSlideToggleModule, MatChipsModule,
     MatSnackBarModule, MatSidenavModule],
   exports: [MatButtonModule, MatCheckboxModule, MatButtonModule, MatIconModule, MatInputModule, 
-    MatSelectModule, MatSliderModule, MatToolbarModule, MatTabsModule,
+    MatSelectModule, MatSliderModule, MatToolbarModule, MatTabsModule, MatChipsModule,
     MatCardModule, MatSlideToggleModule, MatSidenavModule, MatRadioModule,
     MatSnackBarModule],
 })
