6bd876cfa90a8b22aaba4ad698022a1e371e975e
Pablo Velez <pablovv2012@gmail.com>
2018-05-28 01:14:01 +0200

    Adding the angular animations transitions

--- src/app/lists/lists.component.ts
+++ src/app/lists/lists.component.ts
@@ -1,10 +1,35 @@
 import { Component, OnInit } from '@angular/core';
 import { DataService } from '../services/data.service';
 
+import { trigger,style,transition,animate,keyframes,query,stagger } from '@angular/animations';
+
+
 @Component({
   selector: 'app-lists',
   templateUrl: './lists.component.html',
-  styleUrls: ['./lists.component.scss']
+  styleUrls: ['./lists.component.scss'],
+  animations: [
+    trigger('nodes', [
+      transition('* => *', [
+
+        query(':enter', style({ opacity: 0 }), {optional: true}),
+
+        query(':enter', stagger('50ms', [
+          animate('.2s ease-in', keyframes([
+            style({opacity: 0, transform: 'translateY(-75%)', offset: 0}),
+            style({opacity: .5, transform: 'translateY(35px)',  offset: 0.3}),
+            style({opacity: 1, transform: 'translateY(0)',     offset: 1.0}),
+          ]))]), {optional: true})
+          ,
+        query(':leave', stagger('50ms', [
+          animate('.6s ease-out', keyframes([
+            style({opacity: 1, transform: 'translateY(0)', offset: 0}),
+            style({opacity: .5, transform: 'translateY(35px)',  offset: 0.3}),
+            style({opacity: 0, transform: 'translateY(-75%)',     offset: 1.0}),
+          ]))]), {optional: true})
+      ])
+    ])
+  ]
 })
 
 export class ListsComponent implements OnInit {
